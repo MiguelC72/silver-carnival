4b6e3b9ce02bedd3ac2c3d73c03c1fdeea3aa7ba
Miguel Cardenas <c.miguel707@gmail.com>
2016-10-18 14:10:04 -0500

    oops forgot to add the gameOverState file

---
+++ Game/src/game/states/GameOver.java
@@ -0,0 +1,26 @@
+package game.states;
+
+import java.awt.Graphics;
+
+import game.Handler;
+
+public class GameOver extends State {
+
+	public GameOver(Handler handler) {
+		super(handler);
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		System.out.println("kys");
+		
+	}
+
+}
