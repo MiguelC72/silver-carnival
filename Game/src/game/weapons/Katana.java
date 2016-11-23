b420f4f72399210780e46d5aaf56207ea827e29f
Miguel Cardenas <c.miguel707@gmail.com>
2016-11-22 22:12:36 -0600

    added the katana weapon class

---
+++ Game/src/game/weapons/Katana.java
@@ -0,0 +1,25 @@
+package game.weapons;
+
+import game.Handler;
+import game.entities.Entity;
+
+public class Katana extends Weapon {
+
+	public Katana(Handler handler, int width, int height, float x, float y) {
+		super(handler, width, height);
+		
+	}
+
+	@Override
+	public void update(Entity e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onCoolDown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
